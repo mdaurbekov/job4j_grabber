726dd47f82a4528261fdc98b47c8bbf81cbf8062
mdaurbekov <dreamkov@gmail.com>
2022-10-16 23:02:55 +0300

    grabber

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -5,6 +5,8 @@ import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.newJob;
@@ -12,37 +14,43 @@ import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
 import static org.quartz.TriggerBuilder.newTrigger;
 
 public class AlertRabbit {
-    private static int read() {
-        int rezult;
+    private static Properties read() {
         Properties cfg = new Properties();
         try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
             cfg.load(in);
-            rezult = Integer.parseInt(cfg.getProperty("rabbit.interval"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return rezult;
+        return cfg;
     }
 
     public static void main(String[] args) {
         try {
+            List<Long> store = new ArrayList<>();
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDetail job = newJob(Rabbit.class).build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(read())
+                    .withIntervalInSeconds(Integer.parseInt(read().getProperty("rabbit.interval")))
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
+            Thread.sleep(5000);
+            scheduler.shutdown();
+            System.out.println(store);
+        } catch (Exception  se) {
             se.printStackTrace();
         }
     }
 
     public static class Rabbit implements Job {
+
+        public Rabbit() {
+            System.out.println(hashCode());
+        }
         @Override
         public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
