d0962c29ed3e2d92ad77731bb821da60e6b910ae
mdaurbekov <dreamkov@gmail.com>
2022-10-18 20:58:49 +0300

    grabber

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -5,10 +5,12 @@ import java.time.format.DateTimeFormatter;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
 
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+
     @Override
     public LocalDateTime parse(String parse) {
 
-        return LocalDateTime.parse(parse, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+        return LocalDateTime.parse(parse, FORMATTER);
 
     }
 }
