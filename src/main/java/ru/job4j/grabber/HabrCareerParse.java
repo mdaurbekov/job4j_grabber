1faaca8ff99c73a81e2f603592bb9566c77a6e8d
mdaurbekov <dreamkov@gmail.com>
2022-10-19 17:38:23 +0300

    grabber

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,18 +5,38 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
+import ru.job4j.model.Post;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
 
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
 
-    private static final int MAX_NUMBER_PAGES = 1;
+    private static final int MAX_NUMBER_PAGES = 5;
+
+    private final DateTimeParser dateTimeParser;
+    private List<Post> list = new ArrayList<>();
+
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+
+    @Override
+    public List<Post> list(String link) throws IOException {
+        Document document = Jsoup.connect(link).get();
+        String title = document.select(".page-title__title").first().text();
+        LocalDateTime created = dateTimeParser.parse(document.attr("datetime"));
+        list.add(new Post(title, link, retrieveDescription(link), created));
+        return list;
+    }
 
     private static String retrieveDescription(String link) throws IOException {
         String rezult = "";
@@ -42,11 +62,7 @@ public class HabrCareerParse {
                 String vacancyName = titleElement.text();
                 String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
                 System.out.printf("Вакансия: %s, добавлено: %s, ссылка %s%n", vacancyName, data, link);
-                try {
-                    System.out.println(retrieveDescription(link));
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+
             });
         }
 
